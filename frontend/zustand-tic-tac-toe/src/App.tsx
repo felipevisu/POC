490eba1e34ba8ce0e5b86290f4107d048de18590
Felipe Faria <felipevisu@gmail.com>
2025-03-27 00:00:15 -0300

    set winner

--- frontend/zustand-tic-tac-toe/src/App.tsx
+++ frontend/zustand-tic-tac-toe/src/App.tsx
@@ -3,16 +3,18 @@ import { combine } from "zustand/middleware";
 
 import "./App.css";
 
-type SquareValue = string | null;
+type SquareValue = "X" | "O" | null;
 
 type GameBoardState = {
   squares: SquareValue[];
+  xIsNext: boolean;
 };
 
 type GameBoardActions = {
   setSquares: (
     next: SquareValue[] | ((prev: SquareValue[]) => SquareValue[])
   ) => void;
+  setXIsNext: (next: boolean | ((prev: boolean) => boolean)) => void;
 };
 
 type SquareProps = {
@@ -20,8 +22,44 @@ type SquareProps = {
   onSquareClick?: () => void;
 };
 
+function calculateWinner(squares: SquareValue[]): SquareValue {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
+function calculateTurns(squares: SquareValue[]): number {
+  return squares.filter((square) => !square).length;
+}
+
+function calculateStatus(
+  winner: SquareValue,
+  turns: number,
+  player: string
+): string {
+  if (!winner && !turns) return "Draw";
+  if (winner) return `Winner ${winner}`;
+  return `Next player: ${player}`;
+}
+
 const useGameBoard = create<GameBoardState & GameBoardActions>(
-  combine({ squares: Array(9).fill(null) }, (set) => {
+  combine({ squares: Array(9).fill(null), xIsNext: true }, (set) => {
     return {
       setSquares: (nextSquares) => {
         set((state) => ({
@@ -29,6 +67,14 @@ const useGameBoard = create<GameBoardState & GameBoardActions>(
             typeof nextSquares === "function"
               ? nextSquares(state.squares)
               : nextSquares,
+        }));
+      },
+      setXIsNext: (nextXIsNext) => {
+        set((state) => ({
+          xIsNext:
+            typeof nextXIsNext === "function"
+              ? nextXIsNext(state.xIsNext)
+              : nextXIsNext,
         }));
       },
     };
@@ -44,25 +90,36 @@ function Square({ value, onSquareClick }: SquareProps) {
 }
 
 function App() {
-  const { squares, setSquares } = useGameBoard((state) => state);
+  const { squares, xIsNext, setSquares, setXIsNext } = useGameBoard(
+    (state) => state
+  );
+
+  const winner = calculateWinner(squares);
+  const turns = calculateTurns(squares);
+  const player = xIsNext ? "X" : "O";
+  const status = calculateStatus(winner, turns, player);
 
   function handleClick(i: number) {
-    if (squares[i]) return;
+    if (squares[i] || winner) return;
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    nextSquares[i] = player;
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
-    <div className="grid">
-      {squares.map((square, index) => (
-        <Square
-          key={index}
-          value={square}
-          onSquareClick={() => handleClick(index)}
-        />
-      ))}
-    </div>
+    <>
+      <div style={{ marginBottom: "0.5rem" }}>{status}</div>
+      <div className="grid">
+        {squares.map((square, index) => (
+          <Square
+            key={index}
+            value={square}
+            onSquareClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
