cfd5f5e50270813cd4b8c3827db58821e4339d48
Felipe Faria <felipevisu@gmail.com>
2025-05-21 01:00:56 -0300

    add input stories

--- frontend/linaria-ui/src/components/Input/Input.stories.tsx
+++ frontend/linaria-ui/src/components/Input/Input.stories.tsx
@@ -4,6 +4,20 @@ import Input from "./Input";
 const meta: Meta<typeof Input> = {
   component: Input,
   title: "UI/Input",
+  argTypes: {
+    size: {
+      control: {
+        type: "select",
+      },
+      options: ["sm", "md", "lg", "xl"],
+    },
+    error: {
+      control: "boolean",
+    },
+    fullWidth: {
+      control: "boolean",
+    },
+  },
 };
 
 export default meta;
@@ -11,5 +25,8 @@ export default meta;
 export const Default: StoryObj<typeof Input> = {
   args: {
     placeholder: "Enter text...",
+    size: "md",
+    error: false,
+    fullWidth: false,
   },
 };
--- frontend/linaria-ui/src/components/Input/Input.tsx
+++ frontend/linaria-ui/src/components/Input/Input.tsx
@@ -1,14 +1,71 @@
 import { styled } from "@linaria/react";
+import type { Sizes } from "../../types";
+import clsx from "clsx";
+
+const inputSizeMap: Record<Sizes, string> = {
+  sm: "0.25rem 0.5rem",
+  md: "0.5rem 1rem",
+  lg: "0.75rem 1.25rem",
+  xl: "1rem 1.5rem",
+};
+
+const sizeClasses: Record<Sizes, string> = {
+  sm: "input-sm",
+  md: "input-md",
+  lg: "input-lg",
+  xl: "input-xl",
+};
 
 const StyledInput = styled.input`
-  padding: 0.5rem 1rem;
+  all: unset;
   border: 1px solid #ccc;
   border-radius: 6px;
   font-size: 1rem;
+  padding: ${inputSizeMap.md};
+
+  &.input-sm {
+    font-size: 0.8rem;
+    padding: ${inputSizeMap.sm};
+  }
+  &.input-md {
+    padding: ${inputSizeMap.md};
+  }
+  &.input-lg {
+    padding: ${inputSizeMap.lg};
+    font-size: 1.2rem;
+  }
+  &.input-xl {
+    padding: ${inputSizeMap.xl};
+    font-size: 1.5rem;
+  }
+  &.error {
+    border-color: red;
+  }
+  &.fullWidth {
+    width: 100%;
+  }
 `;
 
-const Input = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
-  return <StyledInput {...props} />;
+interface InputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
+  size?: Sizes;
+  error?: boolean;
+  fullWidth?: boolean;
+}
+
+const Input = ({
+  size = "md",
+  error = false,
+  fullWidth = false,
+  className,
+  ...props
+}: InputProps) => {
+  return (
+    <StyledInput
+      className={clsx(sizeClasses[size], { error }, { fullWidth }, className)}
+      {...props}
+    />
+  );
 };
 
 export default Input;
