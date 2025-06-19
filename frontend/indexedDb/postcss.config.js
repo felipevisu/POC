96cdcc303cb2a27ff11a16de39a17e343a83d271
Felipe Faria <felipevisu@gmail.com>
2025-06-19 15:58:26 -0300

    purgecss correct

--- frontend/indexedDb/postcss.config.js
+++ frontend/indexedDb/postcss.config.js
@@ -1,7 +1,12 @@
 import autoprefixer from 'autoprefixer'
+import purgecss from '@fullhuman/postcss-purgecss'
 
 export default {
   plugins: [
     autoprefixer(),
+    purgecss.purgeCSSPlugin({
+      content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,html}'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    })
   ]
 }
