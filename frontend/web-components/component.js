a58fb285f28518b69daecb77b554acbe57cbe116
Felipe Faria <felipevisu@gmail.com>
2025-07-01 23:40:27 -0300

    my first web component

---
+++ frontend/web-components/component.js
@@ -0,0 +1,19 @@
+class MyCircle extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'closed' });
+    this.render(shadow);
+  }
+
+  render(shadow) {
+    const circle = document.createElement('div');
+    circle.classList.add('red-circle');
+    circle.style.width = '100px';
+    circle.style.height = '100px';
+    circle.style.borderRadius = '50%';
+    circle.style.backgroundColor = 'blue';
+    shadow.appendChild(circle);
+  }
+}
+
+customElements.define('my-circle', MyCircle);
