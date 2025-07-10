3b56b2b3a1ca3192c36b4cb4ad0a278317ac3fb5
Felipe Faria <felipevisu@gmail.com>
2025-07-10 00:52:50 -0300

    finally run

--- infra/form-with-aws-infra/infra/main.tf
+++ infra/form-with-aws-infra/infra/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "deployer" {
+resource "aws_key_pair" "terraform" {
   key_name   = "fastapi-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("./fastapi-key.pub")
 }
 
 resource "aws_security_group" "fastapi_sg" {
@@ -33,9 +33,9 @@ resource "aws_security_group" "fastapi_sg" {
 }
 
 resource "aws_instance" "fastapi_server" {
-  ami           = "ami-0c94855ba95c71c99"
+  ami           = "ami-020cba7c55df1f615"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer.key_name
+  key_name      = aws_key_pair.terraform.key_name
   security_groups = [aws_security_group.fastapi_sg.name]
 
   user_data = <<-EOF
@@ -45,12 +45,20 @@ resource "aws_instance" "fastapi_server" {
     systemctl start docker
     systemctl enable docker
 
-    apt-get install -y subversion
+    apt-get install -y git
 
-    svn export https://github.com/felipevisu/POC/trunk/infra/form-with-aws-infra/form /app
+    sudo mkdir -p /app
     cd /app
-    docker build -t fastapi-app .
-    docker run -d -p 8000:8000 fastapi-app
+    sudo git init
+    git config --global --add safe.directory /app
+    git remote add origin https://github.com/felipevisu/POC.git
+    git config core.sparseCheckout true
+    echo infra/form-with-aws-infra/form/ >> .git/info/sparse-checkout
+    git pull origin main
+    cd infra/form-with-aws-infra/form
+
+    sudo docker build -t fastapi-app .
+    sudo docker run -d -p 8000:8000 fastapi-app
   EOF
 
   tags = {
