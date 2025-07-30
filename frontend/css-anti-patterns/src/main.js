9704b3dacc44be60d2cd1887a149371a0f6cd467
Felipe Faria <felipevisu@gmail.com>
2025-07-29 23:16:37 -0300

    css anti patterns

--- frontend/css-anti-patterns/src/main.js
+++ frontend/css-anti-patterns/src/main.js
@@ -1,1 +1,8 @@
-import './style.scss'
+import "./style.scss";
+import hljs from "highlight.js";
+import "highlight.js/styles/atom-one-dark.css";
+
+// Highlight all <pre><code>...</code></pre> blocks
+document.addEventListener("DOMContentLoaded", () => {
+  hljs.highlightAll();
+});
