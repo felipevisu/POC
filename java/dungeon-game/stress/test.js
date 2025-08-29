cada43065b0434c076ea08827a202130bd0bd921
Felipe Faria <felipevisu@gmail.com>
2025-08-29 01:11:22 -0300

    decrease docker

--- java/dungeon-game/stress/test.js
+++ java/dungeon-game/stress/test.js
@@ -2,7 +2,7 @@ import http from "k6/http";
 import { sleep, check } from "k6";
 
 export let options = {
-  vus: 20, // number of concurrent users
+  vus: 200, // number of concurrent users
   duration: "60s", // test duration
 };
 
