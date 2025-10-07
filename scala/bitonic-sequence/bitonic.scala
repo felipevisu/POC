b98ecbd16511a056de803e7ef2971c8f5024a2eb
Felipe Faria <felipevisu@gmail.com>
2025-10-07 00:45:38 -0300

    scala bitonic sequence

---
+++ scala/bitonic-sequence/bitonic.scala
@@ -0,0 +1,34 @@
+//> using scala 3.7.3
+import scala.collection.mutable.ArrayDeque
+
+def bitonicSequence(n: Int, l: Int, r: Int): ArrayDeque[Int] = {
+    val deque = new ArrayDeque[Int]()
+    
+    if (n > (r - l) * 2 * 1){
+        deque.append(-1)
+        return deque
+    }
+
+    deque.append(r - 1)
+
+    var i = r
+    while (i >= l && deque.length < n){
+        deque.append(i)
+        i -= 1
+    }
+
+    i = r - 2;
+    while (i >= l && deque.length < n){
+        deque.prepend(i)
+        i -= 1
+    }
+
+    return deque
+}
+    
+
+
+@main
+def main(): Unit =
+    val seq: ArrayDeque[Int] = bitonicSequence(5, 3, 10)
+    println(seq)
