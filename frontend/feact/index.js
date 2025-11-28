405263b0c0907bcbe977a68ba99da67a49b568d2
Felipe Faria <felipevisu@gmail.com>
2025-11-28 17:11:23 -0300

    render element on dom

--- frontend/feact/index.js
+++ frontend/feact/index.js
@@ -1,18 +1,48 @@
-const myFirstElement = {
-  type: "H1",
-  props: {
-    title: "header",
-    children: "Feact - React By Felipe Faria",
-  },
-};
+function createTextElement(text) {
+  return {
+    type: "TEXT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
 
-const root = document.querySelector("#root");
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
 
-const node = document.createElement(myFirstElement.type);
-node.title = myFirstElement.props.title;
-const text = document.createTextNode("");
-text.nodeValue = myFirstElement.props.children;
+function render(element, container) {
+  const dom =
+    element.type === "TEXT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
 
-node.appendChild(text);
+  Object.keys(element.props)
+    .filter((key) => key !== "children")
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
 
-root.appendChild(node);
+  element.props.children.forEach((child) => render(child, dom));
+
+  container.appendChild(dom);
+}
+
+const element = createElement(
+  "div",
+  { id: "header" },
+  createElement("h1", null, "Feact"),
+  createElement("h2", null, "Felipe Faria React")
+);
+
+const container = document.getElementById("root");
+render(element, container);
