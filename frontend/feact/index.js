fae88e3470b7ef6214869dacef0b566fc1bf2d24
Felipe Faria <felipevisu@gmail.com>
2025-12-10 22:56:21 -0300

    useState

--- frontend/feact/index.js
+++ frontend/feact/index.js
@@ -31,16 +31,46 @@ function render(element, container) {
       ? document.createTextNode("")
       : document.createElement(element.type);
 
+  const isEvent = (key) => key.startsWith("on");
+  const isProperty = (key) => key !== "children" && !isEvent(key);
+
   Object.keys(element.props)
-    .filter((key) => key !== "children")
+    .filter(isProperty)
     .forEach((name) => {
       dom[name] = element.props[name];
+    });
+
+  Object.keys(element.props)
+    .filter(isEvent)
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, element.props[name]);
     });
 
   element.props.children.forEach((child) => render(child, dom));
 
   container.appendChild(dom);
 }
+
+let state = [];
+let stateIndex = 0;
+
+const useState = (initialState) => {
+  const currentIndex = stateIndex;
+
+  if (state[currentIndex] === undefined) {
+    state[currentIndex] = initialState;
+  }
+
+  const setState = (newState) => {
+    state[currentIndex] = newState;
+    stateIndex = 0;
+    reRender();
+  };
+
+  stateIndex++;
+  return [state[currentIndex], setState];
+};
 
 const Feact = {
   createElement,
@@ -49,14 +79,24 @@ const Feact = {
 
 /** @jsx Feact.createElement */
 function Header() {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h1>React</h1>
       <h2>Felipe Faria React Clone</h2>
+      <h3>Count: {count}</h3>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
+
 const element = <Header />;
 
 const container = document.getElementById("root");
 render(element, container);
+
+const reRender = () => {
+  container.innerHTML = "";
+  stateIndex = 0; // Reset state index before re-render
+  render(element, container);
+};
