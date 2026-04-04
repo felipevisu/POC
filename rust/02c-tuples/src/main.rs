d3118811c123d319b718a06fdd9ab69876045bc1
Felipe Faria <felipevisu@gmail.com>
2026-04-04 14:49:15 -0300

    rust rename

--- rust/02c-tuples/src/main.rs
+++ rust/02c-tuples/src/main.rs
@@ -3,10 +3,15 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
     (bool_param, int_param)
 }
 
+struct Matrix(f32, f32, f32, f32);
+
 fn main() {
     let pair = (1, true);
     println!("Pair is {:?}", pair);
 
     let reversed_pair = reverse(pair);
     println!("Reversed pair is {:?}", reversed_pair);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
 }
