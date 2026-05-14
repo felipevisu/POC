43895a505d5eebeab44c80d2e59ff09af8ba365d
Felipe Faria <felipevisu@gmail.com>
2026-05-14 17:38:28 -0300

    rag with ollama

--- ai/rag/main.go
+++ ai/rag/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
+	"sort"
+
+	"github.com/ollama/ollama/api"
 )
 
 
@@ -42,8 +48,82 @@ func loadDocuments() ([]Document, error) {
 	return docs, nil
 }
 
+func generateEmbedding(client *api.Client, text string) ([]float64, error) {
+	req := &api.EmbeddingRequest{
+		Model:  "nomic-embed-text",
+		Prompt: text,
+	}
+
+	resp, err := client.Embeddings(
+		context.Background(),
+		req,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	
+	return resp.Embedding, nil
+}
+
+func cosineSimilarity(a, b []float64) float64 {
+	var dot float64
+	var normA float64
+	var normB float64
+
+	for i := range a {
+		dot += a[i] * b[i]
+		normA += a[i] * a[i]
+		normB += b[i] * b[i]
+	}
+
+	answer := dot / math.Sqrt(normA) * math.Sqrt(normB)
+
+	return answer
+}
+
+func search(client *api.Client, docs []Document, query string) []SearchResult {
+	queryEmbedding, err := generateEmbedding(
+		client,
+		query,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var results []SearchResult
+
+	for _, doc := range docs {
+		score := cosineSimilarity(
+			queryEmbedding,
+			doc.Embedding,
+		)
+
+		results = append(results,
+			SearchResult{
+				Document: doc,
+				Score:    score,
+			},
+		)
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Score >
+			results[j].Score
+	})
+
+	return results
+}
+
 
 func main() {
+	client, err := api.ClientFromEnvironment()
+
+	if err != nil {
+		panic(err)
+	}
+
 	documents, err := loadDocuments()
 
 	if err != nil {
@@ -56,5 +136,34 @@ func main() {
 			"Embedding:",
 			documents[i].Filename,
 		)
+
+		embedding, err := generateEmbedding(client, documents[i].Content)
+
+		if err != nil {
+			panic(err)
+		}
+
+		documents[i].Embedding = embedding
+	}
+
+	for {
+		fmt.Print("Question: ")		
+		reader := bufio.NewReader(os.Stdin)
+		query, _ := reader.ReadString('\n')
+
+		results := search(
+			client,
+			documents,
+			query,
+		)
+
+		for i, result := range results[:3] {
+			fmt.Printf(
+				"%d. %s (%.4f)\n",
+				i+1,
+				result.Document.Filename,
+				result.Score,
+			)
+		}
 	}
 }
